792cc1acc702ea8f0e3626da86e57ed4b3853cd4
florian-shellfire <florian@shellfire.de>
2015-06-24 16:13:31 +0200

    initial commit

---
+++ files/etc/uci-defaults/shellfirebox_init.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+uci batch <<-EOF
+	# set luci theme to Shellfire Box non-advanced/regular mode
+	set luci.main.mediaurlbase=/luci-static/shellfirebox
+	commit luci
+	# set hostname to ShellfireBox
+	set system.@system[0].hostname=ShellfireBox
+	commit system
+EOF
+
+# update hostname to ShellfireBox for immediate effect
+echo $(uci get system.@system[0].hostname) > /proc/sys/kernel/hostname
+
+
+# enable updater cronjob
+/etc/init.d/cron enable
+/etc/init.d/cron start
+/usr/bin/crontab /etc/crontabs/root
+
+# create link (hash) for shellfire-ca.crt
+# hash has been created before with command 
+# HASH=`openssl x509 -hash -noout -in members.dyndns.org.cer`.0
+# see http://wiki.openwrt.org/doc/howto/wget-ssl-certs
+# (needs openssl-util which we do not need to install if hashed upfront)
+ln -s /etc/ssl/certs/shellfire-ca.crt /etc/ssl/certs/2cd70dd3.0
+
+# enable autostart (auto connect if configured) and run once
+/etc/init.d/shellfirebox enable
+/etc/init.d/shellfirebox start
+
---
+++ files/usr/lib/lua/luci/controller/admin/index.lua
@@ -0,0 +1,56 @@
+-- Copyright 2008 Steven Barth <steven@midlink.org>
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.controller.admin.index", package.seeall)
+local debugger = require "luci.debugger"
+
+function index()
+	local root = node()
+	if not root.target then
+		root.target = alias("admin")
+		root.index = true
+	end
+
+	local page   = node("admin")
+	-- page.target  = firstchild()
+	page.target  = alias("admin", "services", "shellfirebox")
+	page.title   = _("Administration")
+	page.order   = 10
+	
+	local shellfirebox = require "luci.shellfirebox"
+	if shellfirebox.isAdvancedMode() then
+		page.sysauth = "root"
+		page.sysauth_authenticator = "htmlauth"
+  	entry({"admin", "logout"}, call("action_logout"), _("Logout"), 90)	
+	else
+	 page.sysauth = false
+	end
+	
+	page.ucidata = true
+	page.index = true
+
+	-- Empty services menu to be populated by addons
+	entry({"admin", "services"}, firstchild(), _("Services"), 40).index = true
+
+
+end
+
+function action_logout()
+  local shellfirebox = require "luci.shellfirebox"
+  if shellfirebox.isAdvancedMode() then
+		local dsp = require "luci.dispatcher"
+		local utl = require "luci.util"
+		local sid = dsp.context.authsession
+	
+		if sid then
+			utl.ubus("session", "destroy", { ubus_rpc_session = sid })
+	
+			dsp.context.urltoken.stok = nil
+	
+			luci.http.header("Set-Cookie", "sysauth=%s; expires=%s; path=%s/" %{
+				sid, 'Thu, 01 Jan 1970 01:00:00 GMT', dsp.build_url()
+			})
+		end
+  end
+	luci.http.redirect(luci.dispatcher.build_url())
+end
