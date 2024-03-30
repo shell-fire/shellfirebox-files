94d3674f4b340ec78cc573ba103661c810fbd751
flo <flo@Debian-Dev-Shellfirebox.myguest.virtualbox.org>
2024-03-30 18:51:14 +0100

    further build improvements

--- build-scripts/04-build-shellfirebox-rom-beryl.sh
+++ build-scripts/04-build-shellfirebox-rom-beryl.sh
@@ -1,63 +1,47 @@
 #!/bin/bash -v
 
-cd ~/beryl/openwrt
-
-rm -rf files
-cp -R ~/beryl/git/shellfirebox-files/files_template files
-
-DATE=`date +%Y-%m`
-SECRETS_FILE="box_secrets_$DATE.txt"
-ROM_DIR="shellfire-boxes-$DATE"
-mkdir ~/beryl/$ROM_DIR
-
-BOX_ID=`ls -1 ~/beryl/$ROM_DIR | wc -l`
-# BOX_ID=$(($BOX_ID / 2))
-BOX_ID=$(($BOX_ID + 55000))
-BOX_ID="sfbox-"$(($BOX_ID + 1))
-
+# Output the generated variables to screen for debug purposes
 echo "ROM_DIR=$ROM_DIR"
 echo "BOX_ID=$BOX_ID"
-echo "SECRETS_FILE=$SECRETS_FILE"
-
-
-
-WIRELESS="files/etc/config/wireless"
-WPA_KEY=`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-9};echo;`
 echo "generated WPA_KEY: $WPA_KEY"
-SSID="Shellfire-Box-"`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-5};echo;`
-echo "generated SSID:"
-echo $SSID
-DEST_FILENAME_FACTORY="$BOX_ID-factory.bin"
-DEST_FILENAME_SYSUPGRADE="$BOX_ID-sysupgrade.bin"
+echo "generated SSID: $SSID"
+echo "DEST_FILENAME_SYSUPGRADE=$DEST_FILENAME_SYSUPGRADE"
+echo "CRON-SCHEDULE AT: $HOUR:$MINUTE"
+echo "SFBOX_UID: $SFBOX_UID"
 echo "DEST_FILENAME_FACTORY=$DEST_FILENAME_FACTORY"
 echo "DEST_FILENAME_SYSUPGRADE=$DEST_FILENAME_SYSUPGRADE"
 
-HOUR=`shuf -i 0-23 -n 1`
-MINUTE=`shuf -i 0-59 -n 1`
-echo "Random daily time for update cron will be $HOUR:$MINUTE"
+# Static variables
+WIRELESS="files/etc/config/wireless"
+NETWORK="files/etc/config/network"
 CRONFILE="files/etc/crontabs/root"
+OPKG_REPO_CONF="files/etc/opkg/customfeeds.conf"
+THE_REPO_PATH="https:\/\/www.shellfire.de\/shellfirebox-repo-23.05.3"
+
+cd ~/beryl/openwrt
+rm -rf files
+cp -R ~/beryl/git/shellfirebox-files/files_template files
+
+# perform substitutions within the files
+# substitutions part 1 - customization for user/specific rom
 sed -i -- "s/THE_HOUR/$HOUR/g" $CRONFILE
 sed -i -- "s/THE_MINUTE/$MINUTE/g" $CRONFILE
+sed -i -- "s/THE_WPA_KEY/$WPA_KEY/g" $WIRELESS
+sed -i -- "s/THE_SSID/$SSID/g" $WIRELESS
+sed -i -- "s/THE_REPO_PATH/$THE_REPO_PATH/g" $OPKG_REPO_CONF
 
 
 #SFBOX_UID=`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-128};echo;`
-SFBOX_UID="mb97quy46kvuvsq38p7wbah6aqm6gykqnrccbw3g2agdxnkw8qwbqkkaz8a92h5c2ph4w4gcdr3bfasyzpfb243b8spdd6hwx6p7phu9zgq35ng5xmz9x26e4mqnc9d3"
-echo "generated UID: $SFBOX_UID"
+# SFBOX_UID="mb97quy46kvuvsq38p7wbah6aqm6gykqnrccbw3g2agdxnkw8qwbqkkaz8a92h5c2ph4w4gcdr3bfasyzpfb243b8spdd6hwx6p7phu9zgq35ng5xmz9x26e4mqnc9d3"
+
+# write the sfboxuid file
+
 echo "$SFBOX_UID" > files/etc/sfboxuid
-
-sed -i -- "s/THE_WPA_KEY/$WPA_KEY/g" $WIRELESS
-sed -i -- "s/THE_SSID/$SSID/g" $WIRELESS
-
-echo "BOX_ID=$BOX_ID	SSID=$SSID	WPA_KEY=$WPA_KEY	FILENAME_FACTORY=$DEST_FILENAME_FACTORY	FILENAME_SYSUPGRADE=$DEST_FILENAME_SYSUPGRADE	SFBOX_UID=$SFBOX_UID" >> ~/$SECRETS_FILE
-
-# make clean
-
 
 make -j1 V=s
 
 
 cp bin/targets/mediatek/filogic/openwrt-mediatek-filogic-glinet_gl-mt3000-squashfs-sysupgrade.bin ~/beryl/$ROM_DIR/$DEST_FILENAME_SYSUPGRADE
 
-echo "generated WPA_KEY: $WPA_KEY"
-echo "DEST_FILENAME_SYSUPGRADE=$DEST_FILENAME_SYSUPGRADE"
+echo "done"
 
--- build-scripts/makeNrom.sh
+++ build-scripts/makeNrom.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
-for i in {1..1000}
+
+if [[ -z "$1" ]]; then
+  NUM_ROMS_TO_BUILD=1
+else
+  NUM_ROMS_TO_BUILD=$1
+fi
+
+for i in {1..$NUM_ROMS_TO_BUILD}
 do
 	# Generate the input for rom generation
 	DATE=`date +%Y-%m`
 	export SECRETS_FILE="box_secrets_$DATE.txt"
 	export ROM_DIR="shellfire-boxes-$DATE"
 
-	export BOX_ID=`ls -1 ~/wt3020h-18.06.4/$ROM_DIR | wc -l`
+	export BOX_ID=`ls -1 ~/beryl/$ROM_DIR | wc -l`
 	export BOX_ID=$(($BOX_ID / 2))
 	export BOX_ID=$(($BOX_ID + 55000))
 	export BOX_ID="sfbox-"$(($BOX_ID + 1))
 
 	export WPA_KEY=`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-9};echo;`
 	export SSID="Shellfire-Box-"`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-5};echo;`
-	export DEST_FILENAME_FACTORY="$BOX_ID-factory.bin"
 	export DEST_FILENAME_SYSUPGRADE="$BOX_ID-sysupgrade.bin"
 	export HOUR=`shuf -i 0-23 -n 1`
 	export MINUTE=`shuf -i 0-59 -n 1`
 
 	#export SFBOX_UID=`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-128};echo;`
-	export SFBOX_UID="axdg3s9xc9bm4pxr6kmyax3yrwe4z9vx2tk7w2dr2n7dcekzp7n9kce3bnwtxd682efg353csn6u8cm5d6hwvu9kq9mfqp8bt2nqtwzrkyvnxm5p8cwnrne6su8zchfa"
-
+	export SFBOX_UID="mb97quy46kvuvsq38p7wbah6aqm6gykqnrccbw3g2agdxnkw8qwbqkkaz8a92h5c2ph4w4gcdr3bfasyzpfb243b8spdd6hwx6p7phu9zgq35ng5xmz9x26e4mqnc9d3"
 	export SSID="Shellfire-Box-"`< /dev/urandom tr -dc abcdefghkmnpqrstuvwxyz23456789 | head -c${1:-5};echo;`
 
 	echo "BOX_ID=$BOX_ID	SSID=$SSID	WPA_KEY=$WPA_KEY	FILENAME_FACTORY=$DEST_FILENAME_FACTORY	FILENAME_SYSUPGRADE=$DEST_FILENAME_SYSUPGRADE	SFBOX_UID=$SFBOX_UID" >> ~/$SECRETS_FILE
 
-	./04-build-shellfirebox-rom.sh 
+	./04-build-shellfirebox-rom-beryl.sh 
 done
