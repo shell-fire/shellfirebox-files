bc47e1b6518f76b8395cbc63213e9df64013f6b1
flo <flo@Debian-Dev-Shellfirebox.myguest.virtualbox.org>
2024-04-02 00:03:33 +0200

    update redirect logic once root pw has been set and ssh activated

--- files_template/usr/sbin/start_dropbear_on_passwd_change.sh
+++ files_template/usr/sbin/start_dropbear_on_passwd_change.sh
@@ -28,7 +28,7 @@ while IFS= read -r line; do
             echo "[start_dropbear_on_passwd_change.sh] Password set for root in $file_changed. Start dropbear, removing dropbear starter from autostart."
             /etc/init.d/dropbear start
             rm /etc/rc.d/S98dropbear_starter_on_passwd_change
-			sed -i 's/\?luci_username=root&luci_password=//g' /www/index.html
+			sed -i 's/?luci_username=root&luci_password=//g' /www/index.html
             break
         else
             # Password is not set
